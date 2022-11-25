d7c59c00596545fcdf00008f3914a83554841bff
Justin Davis <me@justind.dev>
2022-11-25 12:54:22 -0600

    Introducing functional programming

--- 01_simple-example/main.py
+++ 01_simple-example/main.py
@@ -1,12 +1,18 @@
 import json
 from typing import Any, Dict, Iterable
 
+def filter_item_above_threshold(threshold: float, item) -> bool:
+    return item["ppu"] > threshold
+
+def filter_item_type(type: str, item) -> bool:
+    return item["type"] == type
+
 def get_above_threshold_items(data: Dict[str, Any]) -> Iterable[Any]:
     price_threshold = data["price_thresh_now"]
     items = []
 
     for item in data["items"]:
-        if item["ppu"] > price_threshold:
+        if filter_item_above_threshold(price_threshold, item):
             items.append(item)
 
     return items
@@ -15,7 +21,7 @@ def get_items_of_type(data: Dict[str, Any], type: str) -> Iterable[Any]:
     items = []
     
     for item in data["items"]:
-        if item["type"] == type:
+        if filter_item_type(type, item):
             items.append(item)
 
     return items
